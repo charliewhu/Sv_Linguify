975b413cf7969372d7001165c15d331f8c52e08f
Charlie <charlieavery123@hotmail.co.uk>
2023-02-03 21:10:22 +0000

    added form

---
+++ src/routes/texts/create/+page.svelte
@@ -0,0 +1,13 @@
+<title>New Text</title>
+
+<body>
+	<main>
+		<section>
+			<form method="POST">
+				<input type="text" name="name" id="name" placeholder="Name" />
+				<textarea name="body" id="body" placeholder="Body" />
+				<button type="submit"> Create </button>
+			</form>
+		</section>
+	</main>
+</body>
