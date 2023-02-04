7d89430bbf9dd0dfa8217ad783e0bba611f1161a
Charlie <charlieavery123@hotmail.co.uk>
2023-02-04 08:35:31 +0000

    comment

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,5 +1,6 @@
 import { dev } from '$app/environment';
 
+// only run msw in development
 if (dev && 'serviceWorker' in navigator) {
   addEventListener('load', function () {
     navigator.serviceWorker.register('mocks/server.ts');
