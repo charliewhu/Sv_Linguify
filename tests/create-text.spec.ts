6f862341a2bbde3772c2d7c7b8800778b7bdc1ba
Charlie <charlieavery123@hotmail.co.uk>
2023-02-04 06:40:31 +0000

    added visibility assertion

--- tests/create-text.spec.ts
+++ tests/create-text.spec.ts
@@ -5,20 +5,20 @@ const body = "body"
 
 test('Creating a new Text', async ({ page }) => {
   // Given the User is on the NewText page
-  await page.goto('/texts/create/');
-  await expect(page).toHaveTitle(/New Text/);
+  await page.goto('/texts/create/')
+  await expect(page).toHaveTitle(/New Text/)
   
   // When they fill Name content
   // And they fill Body content
   // And they click Submit button
-  await page.getByPlaceholder("name").fill(name);
-  await page.getByPlaceholder("body").fill(body);
-  await page.locator('button[type="submit"]').click();
+  await page.getByPlaceholder("name").fill(name)
+  await page.getByPlaceholder("body").fill(body)
+  await page.locator('button[type="submit"]').click()
   
   // Then they are redirected to the Text detail page
   // And they see the new text name
   // And they see the new text body
   await expect(page).toHaveURL(/texts\/1/);
-  expect(page.getByText(name)).toHaveText(name);
-  expect(page.getByText(body)).toHaveText(body);
+  expect(page.getByText(name)).toBeVisible()
+  expect(page.getByText(body)).toBeVisible()
 });
