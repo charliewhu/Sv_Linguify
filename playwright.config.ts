6ca522af32977c2222a3db34a06d80ebb08eea8f
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 13:02:08 +0000

    formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,36 +9,35 @@ const config: PlaywrightTestConfig = {
 	testDir: 'tests',
 	timeout: 30 * 1000,
 	expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 3000
-  },
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-
+		/**
+		 * Maximum time expect() should wait for the condition to be met.
+		 * For example in `await expect(locator).toHaveText();`
+		 */
+		timeout: 3000
+	},
+	/* Run tests in files in parallel */
+	fullyParallel: true,
+	/* Fail the build on CI if you accidentally left test.only in the source code. */
+	forbidOnly: !!process.env.CI,
+	/* Retry on CI only */
+	retries: process.env.CI ? 2 : 0,
+	/* Opt out of parallel tests on CI. */
+	workers: process.env.CI ? 1 : undefined,
 
 	use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
-    actionTimeout: 0,
-  },
+		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
+		actionTimeout: 0
+	},
 
 	/* Configure projects for major browsers */
 	projects: [
 		{
 			name: 'chromium',
 			use: {
-				...devices['Desktop Chrome'],
-			},
-		},
-	],
+				...devices['Desktop Chrome']
+			}
+		}
+	]
 };
 
 export default config;
