ad561b095bbde59ed45a1cad72805d1ca08ce964
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 08:27:13 +0000

    added enhance to form

--- src/routes/texts/create/+page.svelte
+++ src/routes/texts/create/+page.svelte
@@ -1,8 +1,14 @@
+<script>
+	import { enhance } from '$app/forms';
+</script>
+
 <title>New Text</title>
+
+<h1>Add Todo</h1>
 
 <main>
 	<section class="container">
-		<form method="POST">
+		<form method="POST" use:enhance>
 			<input type="text" name="name" id="name" placeholder="Name" />
 			<textarea name="body" id="body" placeholder="Body" />
 			<button type="submit"> Create </button>
