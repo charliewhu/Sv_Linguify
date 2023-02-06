4130517219b006c17a0b4bb8989a8a692a3eff78
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 11:41:02 +0000

    added request to server and redirect

--- src/routes/texts/create/+page.server.ts
+++ src/routes/texts/create/+page.server.ts
@@ -1,12 +1,28 @@
-import { redirect } from '@sveltejs/kit';
+import { dev } from '$app/environment';
+import { redirect, type Actions } from '@sveltejs/kit';
 
-/** @type {import('./$types').Actions} */
-export const actions = {
-  default: async ({request}) => {
-    //const form = await request.formData()
-    //const name = form.get('name')
-    //const body = form.get('body')
+export const actions: Actions = {
+	default: async ({ request, fetch }) => {
+		const form = await request.formData();
 
-    throw redirect(303, '/texts/1')
-  }
-} 
+		const reqData = {
+			name: form.get('name'),
+			body: form.get('body')
+		};
+
+		// if we're in development then send request to internal server
+		// else send request to external API
+		const url = dev ? '/texts/create' : 'www.apiaddress.com/api/texts/';
+
+		// POST request to API
+		const response = await fetch(url, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(reqData)
+		});
+
+		// redirect to slug/id from response
+		const resData = await response.json();
+		throw redirect(303, `/texts/${resData.id}`);
+	}
+};
