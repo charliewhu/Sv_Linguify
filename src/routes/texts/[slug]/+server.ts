6ad0259ed02349eb9d2dadf59961b351fd779349
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 12:29:00 +0000

    parsed slug

--- src/routes/texts/[slug]/+server.ts
+++ src/routes/texts/[slug]/+server.ts
@@ -4,7 +4,7 @@ export const GET: RequestHandler = async (event) => {
 	// echo back request id
 	// with generic name and body
 	// and 200 OK status code
-	const id = event.params;
+	const id = event.params.slug;
 	const responseData = { id, name: 'name', body: 'body' };
 	const response = json(responseData, { status: 200 });
 	return response;
