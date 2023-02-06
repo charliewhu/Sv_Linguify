1e69e1cb727d38fe9b192f3ebdc28262f74fc0f3
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 08:07:22 +0000

    added ID to response data

--- src/routes/texts/create/+server.ts
+++ src/routes/texts/create/+server.ts
@@ -1,8 +1,11 @@
 import { json, type RequestHandler } from '@sveltejs/kit';
 
 export const POST: RequestHandler = async (event) => {
-	// echo back request data with 201 created status code
+	// echo back request data
+	// with id
+	// and 201 created status code
 	const data = await event.request.json();
-	const response = json(data, { status: 201 });
+	const responseData = await { id: 1, ...data };
+	const response = json(responseData, { status: 201 });
 	return response;
 };
