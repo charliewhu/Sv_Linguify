9e46bebd956c8b1756622cce2be01627c3945bbe
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 11:53:09 +0000

    added handler for getting single Text

---
+++ src/routes/texts/[slug]/+server.ts
@@ -0,0 +1,11 @@
+import { json, type RequestHandler } from '@sveltejs/kit';
+
+export const GET: RequestHandler = async (event) => {
+	// echo back request id
+	// with generic name and body
+	// and 200 OK status code
+	const id = event.params;
+	const responseData = { id, name: 'name', body: 'body' };
+	const response = json(responseData, { status: 200 });
+	return response;
+};
