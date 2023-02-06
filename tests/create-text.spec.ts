79816819b8ef8799f1553b59148ebb468a19f46b
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 06:16:40 +0000

    added first test

--- tests/create-text.spec.ts
+++ tests/create-text.spec.ts
@@ -1,24 +1,24 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-const name = "name"
-const body = "body"
+const name = 'name';
+const body = 'body';
 
 test('Creating a new Text', async ({ page }) => {
-  // Given the User is on the NewText page
-  await page.goto('/texts/create/')
-  await expect(page).toHaveTitle(/New Text/)
-  
-  // When they fill Name content
-  // And they fill Body content
-  // And they click Submit button
-  await page.getByPlaceholder("name").fill(name)
-  await page.getByPlaceholder("body").fill(body)
-  await page.locator('button[type="submit"]').click()
-  
-  // Then they are redirected to the Text detail page
-  // And they see the new text name
-  // And they see the new text body
-  await expect(page).toHaveURL(/texts\/1/);
-  expect(page.getByText(name)).toBeVisible()
-  expect(page.getByText(body)).toBeVisible()
+	// Given the User is on the NewText page
+	await page.goto('/texts/create/');
+	await expect(page).toHaveTitle(/New Text/);
+
+	// When they fill Name content
+	// And they fill Body content
+	// And they click Submit button
+	await page.getByPlaceholder('name').fill(name);
+	await page.getByPlaceholder('body').fill(body);
+	await page.locator('button[type="submit"]').click();
+
+	// Then they are redirected to the Text detail page
+	// And they see the new text name
+	// And they see the new text body
+	await expect(page).toHaveURL(/texts\/1/);
+	expect(page.getByText(name)).toBeVisible();
+	expect(page.getByText(body)).toBeVisible();
 });
