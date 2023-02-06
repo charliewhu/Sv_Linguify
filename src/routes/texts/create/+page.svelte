6c219810370c12b41a442be03952774f63082180
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 13:02:48 +0000

    removed <main> wrapper

--- src/routes/texts/create/+page.svelte
+++ src/routes/texts/create/+page.svelte
@@ -6,12 +6,10 @@
 
 <h1>Add Todo</h1>
 
-<main>
-	<section class="container">
-		<form method="POST" use:enhance>
-			<input type="text" name="name" id="name" placeholder="Name" />
-			<textarea name="body" id="body" placeholder="Body" />
-			<button type="submit"> Create </button>
-		</form>
-	</section>
-</main>
+<section class="container">
+	<form method="POST" use:enhance>
+		<input type="text" name="name" id="name" placeholder="Name" />
+		<textarea name="body" id="body" placeholder="Body" />
+		<button type="submit"> Create </button>
+	</form>
+</section>
