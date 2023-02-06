8a5935240b899e8924ec48a7c0d24b26ca8725b6
Charlie <charlieavery123@hotmail.co.uk>
2023-02-06 19:46:33 +0000

    changed url and returned texts array within object

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,10 +3,10 @@ import type { PageServerLoad } from './texts/[slug]/$types';
 
 export const load: PageServerLoad = async ({ fetch }) => {
 	// only call real API if not in development
-	const url = dev ? `/texts/` : 'www.apiaddress.com/api/texts/';
+	const url = dev ? `/` : 'www.apiaddress.com/api/texts/';
 
 	const response = await fetch(url);
 	const data = await response.json();
 
-	return data;
+	return { texts: data };
 };
