e5bffafb860b3b45ca3ea88365f5c861600e41ae
Charlie <charlieavery123@hotmail.co.uk>
2023-02-07 21:42:26 +0000

    added navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,15 @@
+<nav class="container-fluid">
+	<ul>
+		<li>
+			<a class="contrast" href="/"><strong>Linguify</strong></a>
+		</li>
+	</ul>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/texts/create">Create Text</a></li>
+	</ul>
+</nav>
+
 <main class="container">
 	<slot />
 </main>
